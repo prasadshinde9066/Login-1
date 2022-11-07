57d9346e0a999317e38b612a8131612f6030be4b
prasadshinde9066 <prasadshinde14256@gmail.com>
2022-11-07 20:46:22 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const togglepassword = button =>{
+    button.classList.toggle("showing")
+
+    const input =
+    document.getElementById("password")
+
+    input.type =
+    input.type == "password"
+    ? "text"
+    : "password"
+}
